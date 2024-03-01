54cc4484873d899d4446a33ea70b4953c11373ad
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2024-03-02 00:14:06 +0530

    change of controller

--- Controllers/mcqcontroller.js
+++ Controllers/mcqcontroller.js
@@ -1,28 +1,50 @@
 //for better organization we use model - controller architecture
-const MCQ=require('../models/mcqschema.js')
-const mongoose=require('mongoose')
+const MCQ = require("../models/mcqschema.js");
+const mongoose = require("mongoose");
 
 //get all mcqs
-const getMcqs= async(req,res)=>{
-    const mcqs=await MCQ.find({}).sort({createdAt:-1})
-    res.status(200).json(mcqs)
-}
+const getMcqs = async (req, res) => {
+  const projection = {
+    questionid: 1,
+    question: 1,
+    option1: 1,
+    option2: 1,
+    option3: 1,
+    option4: 1,
+  };
+  const mcqs = await MCQ.find({},projection).sort({ createdAt: 1 });
+  res.status(200).json(mcqs);
+};
 
 //create new mcq
-const createMcq=async (req,res)=>{
-    const {questionid,question,option1,option2,option3,option4,correct_option}=req.body
-    //add mcq to db
-    try{
-        const mcq=await MCQ.create({questionid,question,option1,option2,option3,option4,correct_option})
-        res.status(200).json(mcq)
-    }
-    catch(error)
-    {
-        res.status(400).json({error:error.message})
-    }
-}
+const createMcq = async (req, res) => {
+  const {
+    questionid,
+    question,
+    option1,
+    option2,
+    option3,
+    option4,
+    correct_option,
+  } = req.body;
+  //add mcq to db
+  try {
+    const mcq = await MCQ.create({
+      questionid,
+      question,
+      option1,
+      option2,
+      option3,
+      option4,
+      correct_option,
+    });
+    res.status(200).json(mcq);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-module.exports ={
-    getMcqs,
-    createMcq,
-}
+module.exports = {
+  getMcqs,
+  createMcq,
+};
